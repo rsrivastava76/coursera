0fc6512873a78bbcfc89f2d906ab3a3b21135c76
Ritesh Srivastava <rsrivastava76@hotmail.com>
2017-03-16 21:53:27 +0530

    understanding of scope and calling

--- lacture1/app.js
+++ lacture1/app.js
@@ -1,9 +1,13 @@
 (function () {
+  'use strict';
 
 angular.module('myFirstApp',[])
 
-.controller('MyFirstController', function(){
-
+.controller('MyFirstController', function($scope){
+$scope.name = "Ritesh Srivastava";
+$scope.sayHello = function() {
+  return "Hello Coursera!!!";
+};
 });
 
 })();
